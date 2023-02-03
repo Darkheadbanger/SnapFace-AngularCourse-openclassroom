fee90204ee0f7626e4322b29f3cebe40fb14b944
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-02-03 12:59:39 +0100

    creation de la propriété personalisée et l'injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'snapFace';
-}
+export class AppComponent {}
