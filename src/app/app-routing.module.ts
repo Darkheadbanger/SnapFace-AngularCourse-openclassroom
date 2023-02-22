099bb92a6e94263c1d50bef38d14bde1cfadcd97
David Bouhaben <davidbouhaben@yahoo.co.id>
2023-02-22 12:59:42 +0100

    creation de routing angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
+import { LandingPageComponentComponent } from './landing-page-component/landing-page-component.component';
 
 const routes: Routes = [{
-  path: "faceSnaps", component: FaceSnapListComponent
+  path: "faceSnaps", component: FaceSnapListComponent,
+},{
+  path: "", component: LandingPageComponentComponent
 }]
 @NgModule({
   declarations: [],
